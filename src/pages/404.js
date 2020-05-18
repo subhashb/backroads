2229b5b5e3244501bd2210d88961cd2192ae0cf8
Subhash Bhushan <subhash.bhushan@gmail.com>
2020-05-17 18:15:47 -0700

    Style Error page and add styled components

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,21 @@
-import React, { Component } from "react"
+import React from "react"
+import Layout from "../components/Layout"
+import styles from "../css/error.module.css"
+import { Link } from "gatsby"
+import Banner from "../components/Banner"
 
-export default class Error extends Component {
-  render() {
-    return <div>Hello from Error page!!!</div>
-  }
+const Error = () => {
+  return (
+    <Layout>
+      <header className={styles.error}>
+        <Banner title="oops it's a dead end">
+          <Link to="/" className="btn-white">
+            Back to home page
+          </Link>
+        </Banner>
+      </header>
+    </Layout>
+  )
 }
+
+export default Error
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,9 +1,17 @@
 import React from "react"
 
 import Layout from "../components/Layout"
+import Button from "../examples/Button"
 
 const tours = () => {
-  return <Layout>Hello from Tours Page!!!</Layout>
+  return (
+    <Layout>
+      Hello from Tours Page!!!
+      <div>
+        <Button>hello from styled</Button>
+      </div>
+    </Layout>
+  )
 }
 
 export default tours
