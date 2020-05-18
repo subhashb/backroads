090b837a878d6c73cd526f5bb8bed0a42836f06f
Subhash Bhushan <subhash.bhushan@gmail.com>
2020-05-18 12:57:55 -0700

    Services section in Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import SimpleHero from "../components/SimpleHero"
 import Banner from "../components/Banner"
 import { Link } from "gatsby"
 import About from "../components/Home/About"
+import Services from "../components/Home/Services"
 
 export default () => (
   <Layout>
@@ -18,5 +19,6 @@ export default () => (
       </Banner>
     </SimpleHero>
     <About />
+    <Services />
   </Layout>
 )
