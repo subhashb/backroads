529f69a2ef46eb0741a9b3b96b1d561d1cbcff0a
Subhash Bhushan <subhash.bhushan@gmail.com>
2020-05-23 13:26:21 -0700

    Styled Hero for all pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,26 @@
 import React from "react"
 import Layout from "../components/Layout"
+import StyledHero from "../components/StyledHero"
+import { graphql } from "gatsby"
 
-const contact = () => {
-  return <Layout>Contact Page</Layout>
+const contact = ({ data }) => {
+  return (
+    <Layout>
+      <StyledHero img={data.connectBcg.childImageSharp.fluid}></StyledHero>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query {
+    connectBcg: file(relativePath: { eq: "connectBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
 
 export default contact
