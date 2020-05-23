ebb1df3dc47403180718e8037d441c2f800987bc
Subhash Bhushan <subhash.bhushan@gmail.com>
2020-05-23 12:27:57 -0700

    Use Gatsby Image in About Component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import Images from "../examples/Images"
 
 const blog = () => {
   return (
     <Layout>
       Hello from Blog Page!!!
       <Link to="/">Home</Link>
-      <Images />
     </Layout>
   )
 }
