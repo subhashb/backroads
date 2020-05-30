16c03878d30b28950ab4d4190a3a6f4459ae11bf
Subhash Bhushan <subhash.bhushan@gmail.com>
2020-05-29 21:58:52 -0700

    Setup SEO for all pages

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -3,10 +3,12 @@ import Layout from "../components/Layout"
 import StyledHero from "../components/StyledHero"
 import { graphql } from "gatsby"
 import Tours from "../components/Tours/Tours"
+import SEO from "../components/SEO"
 
 const tours = ({ data }) => {
   return (
     <Layout>
+      <SEO title="Tours" />
       <StyledHero img={data.defaultBcg.childImageSharp.fluid}></StyledHero>
       <Tours />
     </Layout>
--- src/templates/tour-template.js
+++ src/templates/tour-template.js
@@ -7,6 +7,7 @@ import Img from "gatsby-image"
 import { FaMoneyBillWave, FaMap } from "react-icons/fa"
 import Day from "../components/SingleTour/Day"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
+import SEO from "../components/SEO"
 
 export const getTour = graphql`
   query($slug: String!) {
@@ -48,6 +49,7 @@ const Template = ({ data }) => {
 
   return (
     <Layout>
+      <SEO title={name} />
       <StyledHero img={mainImage.fluid} />
       <section className={styles.template}>
         <div className={styles.center}>
